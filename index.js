db27a6f6eb1da69765c6f752e39388e9970c6c92
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-04-01 16:44:44 -0700

    Control flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const { expect } = require("chai");
+
+function scuberGreetingForFeet(value){
+  if(400 >= value){
+    return "This one is on me!";
+  } else if (2000 >= value && 400 <= value){
+    return "That will be twenty bucks.";
+} else if (2001 >= value){
+      return "I will gladly take your thirty bucks.";
+  } else if (2501 >= value){
+      return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+  const city = 'NYC';
+  return (isCity === city ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+    default:
+  }
+
 }
